9ce8edd632e396d170e59f37c26eed651f5960d8
Yuvraj <yuvrajdjadhav@gmail.com>
2022-09-26 11:47:41 +0530

    int

---
+++ application/logs/log-2022-09-26.php
@@ -0,0 +1,21 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+INFO - 2022-09-26 11:42:14 --> Config Class Initialized
+INFO - 2022-09-26 11:42:14 --> Hooks Class Initialized
+DEBUG - 2022-09-26 11:42:14 --> UTF-8 Support Enabled
+INFO - 2022-09-26 11:42:14 --> Utf8 Class Initialized
+INFO - 2022-09-26 11:42:14 --> URI Class Initialized
+DEBUG - 2022-09-26 11:42:14 --> No URI present. Default controller set.
+INFO - 2022-09-26 11:42:14 --> Router Class Initialized
+INFO - 2022-09-26 11:42:14 --> Output Class Initialized
+INFO - 2022-09-26 11:42:14 --> Security Class Initialized
+DEBUG - 2022-09-26 11:42:14 --> Global POST, GET and COOKIE data sanitized
+INFO - 2022-09-26 11:42:14 --> Input Class Initialized
+INFO - 2022-09-26 11:42:14 --> Language Class Initialized
+INFO - 2022-09-26 11:42:14 --> Loader Class Initialized
+INFO - 2022-09-26 11:42:14 --> Helper loaded: url_helper
+INFO - 2022-09-26 11:42:14 --> Helper loaded: form_helper
+INFO - 2022-09-26 11:42:15 --> Database Driver Class Initialized
+ERROR - 2022-09-26 11:42:15 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'u309911761_webdemo'@'localhost' (using password: YES) /var/www/html/GIR/system/database/drivers/mysqli/mysqli_driver.php 201
+ERROR - 2022-09-26 11:42:15 --> Unable to connect to the database
+INFO - 2022-09-26 11:42:15 --> Language file loaded: language/english/db_lang.php
